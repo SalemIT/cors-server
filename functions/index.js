f43b6c7d2435f1eab77981a0f74abf2f789b0c27
taocode <mark@taocode.com>
2023-06-25 03:38:20 -0400

    feat(works): cors anywhere via firebase function

--- functions/index.js
+++ functions/index.js
@@ -6,10 +6,12 @@ const corsServer = corsAnywhere.createServer({
     originWhitelist: [
       'http://localhost:3000',
       'http://localhost:5000',
-      'https://portfolio-ravenous.web.app',
-      'https://portfolio-ravenous.firebaseapp.com',
-      'https://test-my-api-endpoint.web.app',
-      'https://test-my-api-endpoint.firebaseapp.com'
+      'https://salemprivatecors.web.app',
+      'https://salemprivatecors.firebaseapp.com',
+      // 'https://test-my-api-endpoint.web.app',
+      // 'https://test-my-api-endpoint.firebaseapp.com',
+      'https://sites.google.com',
+      'https://studio.webcomponents.dev',
     ],
     requireHeader: ['origin', 'x-requested-with'],
     removeHeaders: ['cookie', 'cookie2']
